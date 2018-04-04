7d9f539e6a4eb227fbf84d5f2095d42cf7830c52
Derek <bver49@gmail.com>
2018-04-04 21:48:11 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,14 +2,11 @@ var helper = require('./helper');
 var getOrderDetail = helper.getOrderDetail;
 var getOrderIncome = helper.getOrderIncome;
 var key = {
-  shopid:"",
-  partner_id:"",
-  shopeesecret:""
+    shopeeshopid:"",
+    shopeepartnerid:"",
+    shopeesecret:""
 }
 
 getOrderDetail("ordersn",key,function(detail){
-  getOrderIncome("ordersn",key,function(income){
     console.log(detail);
-    console.log(income);
-  });
 });
