e18b2d8a86707902c36263b174fd6787d1d728b7
Derek <derek@playsport.cc>
2018-04-20 11:56:33 +0800

    Update

--- helper/yahoo.js
+++ helper/yahoo.js
@@ -1,18 +1,18 @@
 var request = require('request');
 var crypto = require('crypto');
-var yahooAPIkey = "";
-var yahooAPISecret = "";
+var yahooAPIkey = config.yahoo.APIKey;
+var yahooAPISecret = config.yahoo.APISecret;
 
 function callAPI(url, data){
     return new Promise(function(resolve, reject){
         var ts = Math.floor(new Date().getTime() / 1000);
-        var QueryString = "&Format=json";
+        var QueryString = "";
         if (typeof data !== "undefined" && Object.keys(data).length > 0) {
             for (var i in data){
                 QueryString += `&${i}=${data[i]}`;
             }
         }
-        var RequestContent = `ApiKey=${yahooAPIkey}&TimeStamp=${ts}${QueryString}`;
+        var RequestContent = `ApiKey=${yahooAPIkey}&TimeStamp=${ts}&Format=json${QueryString}`;
         var Signature = crypto.createHmac('sha1', yahooAPISecret).update(RequestContent).digest('hex');
         url = url + "?" + RequestContent + "&Signature=" + Signature;
         request({
